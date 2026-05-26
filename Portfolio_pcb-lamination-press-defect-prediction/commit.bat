49e132cfeaa454dde8a7557f70675b37284a00a2
songgongho <songgongho@gmail.com>
2026-05-26 11:03:30 +0900

    Update: add remaining changes

---
+++ Portfolio_pcb-lamination-press-defect-prediction/commit.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "E:\2026-1학기\Industrial_AI\Portfolio_pcb-lamination-press-defect-prediction"
+git commit -m "Update: Add literature review framework and quality dashboard"
+git push origin main
+pause
+
